0fd073647d2335643792f3016d9e4d4f592d07c3
phfr24 <phfr24@inf.ufpr.br>
2026-05-28 00:12:16 -0300

    add qwerty/colemak layout swapping

--- keyboards/cheapino/keymaps/colemak/config.h
+++ keyboards/cheapino/keymaps/colemak/config.h
@@ -4,3 +4,7 @@
 #define TAPPING_TERM 165
 #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
+
+/* macos sleep power mgmt quirks?? */
+#define NO_USB_STARTUP_CHECK
+#define USB_SUSPEND_WAKEUP_DELAY 0
--- keyboards/cheapino/keymaps/colemak/keymap.c
+++ keyboards/cheapino/keymaps/colemak/keymap.c
@@ -2,6 +2,7 @@
 
 enum layers
 {
+  L_QWERTY,
   L_COLEMAK,
   L_NUM,
   L_SYM,
@@ -15,11 +16,19 @@ enum custom_keycodes
   MACRO_BRACKETS,
   MACRO_QUOTES,
   MACRO_ARROW,
-  MACRO_ANGLES
+  MACRO_ANGLES,
+  TG_BASE, /* toggle base layer between colemak and qwerty */
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS]
-    = { [L_COLEMAK] = LAYOUT_split_3x5_3 (
+    = { [L_QWERTY] = LAYOUT_split_3x5_3 (
+            KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+            KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
+            LSFT_T (KC_Z), LCTL_T (KC_X), KC_C, KC_V, KC_B, KC_N, KC_M,
+            RGUI_T (KC_COMM), RALT_T (KC_DOT), RSFT_T (KC_SLSH), KC_SPC,
+            MO (L_SYM), KC_ENT, KC_ESC, MO (L_NUM), KC_BSPC),
+
+        [L_COLEMAK] = LAYOUT_split_3x5_3 (
             KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
             KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O,
             LSFT_T (KC_Z), LCTL_T (KC_X), KC_C, KC_V, KC_B, KC_K, KC_M,
@@ -46,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS]
             KC_F10, KC_BRID, KC_BRIU, KC_CALC, KC_WSCH, KC_WBAK, KC_F13,
             KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_UNDO, KC_CUT, KC_COPY,
             KC_PSTE, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_TRNS, QK_BOOT, KC_NO, KC_TRNS, KC_NO) };
+            KC_TRNS, KC_NO, TG_BASE, KC_TRNS, KC_NO) };
 
 bool
 process_record_user (uint16_t keycode, keyrecord_t *record)
@@ -73,6 +82,13 @@ process_record_user (uint16_t keycode, keyrecord_t *record)
           return false;
         case MACRO_ANGLES:
           SEND_STRING ("<>" SS_TAP (X_LEFT));
+          return false;
+        case TG_BASE:
+          if (default_layer_state & (1UL << L_COLEMAK))
+            set_single_persistent_default_layer(L_QWERTY);
+          else
+            set_single_persistent_default_layer(L_COLEMAK);
+
           return false;
         }
     }
